d3e44fbf80851197ea19bf8e090b48afe151e447
NutNuke <128171822+NutNuke@users.noreply.github.com>
2023-05-05 23:36:59 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+//index 페이지에서 달력과 TeamName을 불러오는 함수
+//AJAX사용
+function LoadCalendar() {
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("Content").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "https://nutnuke.github.io/calendar/CalendarIndex.html", true);
+  xhttp.send();
+}
+
+function LoadTeamName() {
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("Content").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "https://nutnuke.github.io/calendar/schedule.html", true);
+  xhttp.send();
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,13 @@
-this is my file!
+UPDATE NOTES </br>
+</br>
+03/28 : START </br>
+04/04 : 달력 추가/달력에서 날짜 클릭시 schedule page로 넘어갑니다/데이터 받아올 schedule page 추가 </br>
+04/06 : index page 정리 및 700px로 통일 </br>
+04/24 : calendar page - html 과 js 분리 및 js 파일 정리</br>
+&nbsp;달력 클릭시 schedule page에 선택한 날짜값의 query가 전달되도록 수정</br>
+&nbsp;schedule page에서 선택한 날짜를 볼수 있게 수정</br>
+04/26 : 선택한 날짜에 따라 각 날짜에 맞는 schedule 데이터를 볼 수 있게 추가
+<br/>&nbsp; schedule.json 파일 추가<br/>
+05/03 : 메인 페이지 및 달력 디자인 수정(미완) 메인페이지에서 당일의 일정을 볼 수 있게 추가
+<br/>&nbsp; 메인페이지 css,js분리
+<br/>&nbsp; 달력 출력방식 일부 수정
