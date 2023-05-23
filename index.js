0b01312fcaa4486586a26f29229445bc9d9d321a
NutNuke <128171822+NutNuke@users.noreply.github.com>
2023-05-23 12:44:08 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -2,17 +2,30 @@
 //AJAX사용
 src = "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"; //jquery, Ajax 불러오기
 
-//로드 by jquery
-$.ajax({
-  url: "https://nutnuke.github.io/calendar/calendar.html",
-  dataType: "html",
-  success: function (response) {
-    // 로드된 HTML을 삽입하면서 자동으로 <script>가 실행됩니다.
-    $("#Content").html(response);
-  },
-});
+function LoadCalendar() {
+  //로드 by jquery
+  $.ajax({
+    url: "https://nutnuke.github.io/calendar/calendar.html",
+    dataType: "html",
+    success: function (response) {
+      // 로드된 HTML을 삽입하면서 자동으로 <script>가 실행됩니다.
+      $("#Content").html(response);
+    },
+  });
+}
+function LoadSchedule() {
+  //로드 by jquery
+  $.ajax({
+    url: "https://nutnuke.github.io/calendar/schedule.html",
+    dataType: "html",
+    success: function (response) {
+      // 로드된 HTML을 삽입하면서 자동으로 <script>가 실행됩니다.
+      $("#Content").html(response);
+    },
+  });
+}
 
-// //로드함수
+// //로드함수 (오류발생)
 // let xhttp = new XMLHttpRequest();
 // xhttp.onreadystatechange = function () {
 //   //콜백함수
