dcbc1bbecf5e3fe2fc8c323085ea6b575b1e04f5
NutNuke <128171822+NutNuke@users.noreply.github.com>
2023-06-24 02:08:04 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //index 페이지에서 달력과 TeamName을 불러오는 함수
 //AJAX사용
-src = "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"; //jquery, Ajax 불러오기
+//jquery, Ajax 불러오기
 
 function LoadCalendar() {
   //로드 by jquery
